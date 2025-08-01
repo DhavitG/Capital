7ff7dd4f9bdacf9f13e0f7ddfab8fd34202129c9
Dhavit Gandhi <gdhavit@gmail.com>
2025-08-01 18:42:08 +0530

    Clients Component

--- src/Components/Clients.jsx
+++ src/Components/Clients.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { clients } from "../constants";
+import styles from "../styles";
 
 const Clients = () => {
-  return <div></div>;
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((client) => {
+          return (
+            <div
+              key={client.id}
+              className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[128px]`}
+            >
+              <img
+                src={client.logo}
+                alt="client"
+                className="sm:w-[192px] w-[100px] object-contain"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Clients;
